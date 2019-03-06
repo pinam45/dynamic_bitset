dad356368a3824188d4d89d36f49f15775cb1957
Maxime Pinard <maxime.pin@live.fr>
2019-03-06 12:57:31 -0500

    tests: Added tests configuration

---
+++ tests/src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main()
+#include <catch2/catch.hpp>
