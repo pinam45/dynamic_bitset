7ed0e3fae75963102a5d6f2918cef151027b6ec7
Maxime Pinard <maxime.pin@live.fr>
2019-05-18 15:19:21 -0400

    tests: Added find_first find_next tests with empty bitset

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1344,20 +1344,34 @@ TEMPLATE_TEST_CASE("find_first find_next",
                    uint64_t)
 {
 	CAPTURE(SEED);
-	const std::tuple<size_t, size_t> values =
-	  GENERATE(multitake(RANDOM_VECTORS_TO_TEST,
-	                     randomInt<size_t>(0, 5 * bits_number<TestType>, SEED),
-	                     randomInt<size_t>(0, 5 * bits_number<TestType>, SEED + 1)));
-	const size_t first_bit_pos = std::get<0>(values);
-	const size_t second_bit_pos = first_bit_pos + 1 + std::get<1>(values);
-	CAPTURE(first_bit_pos, second_bit_pos);
 
-	dynamic_bitset<TestType> bitset(second_bit_pos + 12);
-	bitset.set(first_bit_pos);
-	bitset.set(second_bit_pos);
-	REQUIRE(bitset.find_first() == first_bit_pos);
-	REQUIRE(bitset.find_next(first_bit_pos) == second_bit_pos);
-	REQUIRE(bitset.find_next(second_bit_pos) == bitset.npos);
+	SECTION("empty-bitset")
+	{
+		dynamic_bitset<TestType> bitset;
+
+		REQUIRE(bitset.find_first() == bitset.npos);
+		REQUIRE(bitset.find_next(0) == bitset.npos);
+	}
+
+	SECTION("non-empty bitset")
+	{
+		const std::tuple<size_t, size_t> values =
+		  GENERATE(multitake(RANDOM_VECTORS_TO_TEST,
+		                     randomInt<size_t>(0, 5 * bits_number<TestType>, SEED),
+		                     randomInt<size_t>(0, 5 * bits_number<TestType>, SEED + 1)));
+		const size_t first_bit_pos = std::get<0>(values);
+		const size_t second_bit_pos = first_bit_pos + 1 + std::get<1>(values);
+		CAPTURE(first_bit_pos, second_bit_pos);
+
+		dynamic_bitset<TestType> bitset(second_bit_pos + 12);
+		REQUIRE(bitset.find_first() == bitset.npos);
+		bitset.set(first_bit_pos);
+		bitset.set(second_bit_pos);
+		REQUIRE(bitset.find_first() == first_bit_pos);
+		REQUIRE(bitset.find_next(first_bit_pos) == second_bit_pos);
+		REQUIRE(bitset.find_next(second_bit_pos) == bitset.npos);
+		REQUIRE(bitset.find_next(bitset.size()) == bitset.npos);
+	}
 }
 
 TEMPLATE_TEST_CASE("swap", "[dynamic_bitset]", uint16_t, uint32_t, uint64_t)
