58e98071aac96e90dac708f9b107bb0d0ccc3532
Maxime Pinard <maxime.pin@live.fr>
2026-09-15 19:58:45 +0200

    Simplify CMake formatting targets generation

--- cmake/modules/sul-format.cmake
+++ cmake/modules/sul-format.cmake
@@ -6,7 +6,7 @@ function(sul_generate_format_target_from_files target_name)
     cmake_parse_arguments(
       sul_generate_format_target_from_files
       ""
-      "BASE_TARGETS_FOLDER"
+      ""
       "FILES"
       ${ARGN}
     )
@@ -17,12 +17,6 @@ function(sul_generate_format_target_from_files target_name)
     # Checks
     if(TARGET ${target_name})
         message(FATAL_ERROR "${target_name} already exists")
-    endif()
-
-    # Setup targets forder prefix
-    set(targets_folder_prefix)
-    if(sul_generate_format_target_from_files_BASE_TARGETS_FOLDER)
-        set(targets_folder_prefix "${sul_generate_format_target_from_files_BASE_TARGETS_FOLDER}/")
     endif()
 
     # Find clang-format
@@ -39,29 +33,16 @@ function(sul_generate_format_target_from_files target_name)
       WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
       VERBATIM
     )
-    set_target_properties(${target_name} PROPERTIES FOLDER "${targets_folder_prefix}format")
     message(STATUS "Generated format target ${target_name}")
 
     # Add dependency to format-all
     if(NOT TARGET format-all)
         add_custom_target(format-all)
-        set_target_properties(format-all PROPERTIES FOLDER "${targets_folder_prefix}format")
     endif()
     add_dependencies(format-all ${target_name})
 endfunction()
 
 function(sul_generate_format_target_from_target target)
-    cmake_parse_arguments(
-      arg_sul_generate_format_target_from_target
-      ""
-      "BASE_TARGETS_FOLDER"
-      ""
-      ${ARGN}
-    )
-    if(arg_sul_generate_format_target_from_target_UNPARSED_ARGUMENTS)
-        message(FATAL_ERROR "Unexpected arguments: ${arg_sul_generate_format_target_from_target_UNPARSED_ARGUMENTS}")
-    endif()
-
     # Checks
     if(NOT TARGET ${target})
         message(FATAL_ERROR "Invalid argument: ${target} is not a target")
@@ -85,7 +66,6 @@ function(sul_generate_format_target_from_target target)
     # Generate target from files
     sul_generate_format_target_from_files(
       ${format-target}
-      BASE_TARGETS_FOLDER "${arg_sul_generate_format_target_from_target_BASE_TARGETS_FOLDER}"
       FILES ${target_sources}
     )
 endfunction()
